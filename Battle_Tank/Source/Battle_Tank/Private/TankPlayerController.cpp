ca778a174a8749bbb8998c210e4ad4b09f445dd8
DESKTOP-B0PFM30\MhKh <mhkh431@gamil.com>
2018-11-13 23:01:48 +0330

    Battle Tank AIPlayerController

--- Battle_Tank/Source/Battle_Tank/Private/TankPlayerController.cpp
+++ Battle_Tank/Source/Battle_Tank/Private/TankPlayerController.cpp
@@ -3,6 +3,21 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	auto Controlledtank = GetControlledTank();
+	if (!Controlledtank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possseing a tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s"), *(Controlledtank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
