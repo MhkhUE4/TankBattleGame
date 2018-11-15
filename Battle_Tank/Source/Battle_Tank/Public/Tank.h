184843a142149771172191eacee3982aca76e828
DESKTOP-B0PFM30\MhKh <mhkh431@gamil.com>
2018-11-15 14:42:08 +0330

    BAttle Tank Subobject

--- Battle_Tank/Source/Battle_Tank/Public/Tank.h
+++ Battle_Tank/Source/Battle_Tank/Public/Tank.h
@@ -2,9 +2,10 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // Put new includes above
 
 UCLASS()
 class BATTLE_TANK_API ATank : public APawn
@@ -14,15 +15,16 @@ class BATTLE_TANK_API ATank : public APawn
 public:
 	void AimAt(FVector HitLocation);
 
-private:
-	// Sets default values for this pawn's properties
-	ATank();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:	
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+private:
+	// Sets default values for this pawn's properties
+	ATank();
+	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
