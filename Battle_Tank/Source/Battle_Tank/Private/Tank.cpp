876d8feaf178ed23c54354b1008eba8e0a29042a
DESKTOP-B0PFM30\MhKh <mhkh431@gamil.com>
2018-11-29 13:49:35 +0330

    Battle Tank Tanks Fly OFF

--- Battle_Tank/Source/Battle_Tank/Private/Tank.cpp
+++ Battle_Tank/Source/Battle_Tank/Private/Tank.cpp
@@ -2,6 +2,7 @@
 
 #include "Tank.h"
 #include "TankAimingComponent.h"
+#include "Engine/World.h"
 
 
 
@@ -23,6 +24,12 @@ void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
 {
 	TankAimingComponent->SetTurretReference(TurretToSet);
+}
+
+void ATank::Fire()
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Tank Is Firing"), Time);
 }
 
 // Called when the game starts or when spawned
