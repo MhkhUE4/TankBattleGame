da6daf72a427620a9c25a6f9a551786f784cc15b
DESKTOP-B0PFM30\MhKh <mhkh431@gamil.com>
2018-11-29 23:46:11 +0330

    Battle Tank Coloring Cross Hair

--- Battle_Tank/Source/Battle_Tank/Public/TankAimingComponent.h
+++ Battle_Tank/Source/Battle_Tank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState: uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 //Forward Declaration
 class UTankBarrel; 
 class UTankTurret;
@@ -23,8 +32,11 @@ public:
 
 	void SetTurretReference(UTankTurret* TurretToSet);
 
-public:	
 	void AimAt(FVector HitLocation, float LaunchSpeed);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
 
 private:
 	UTankBarrel* Barrel = nullptr;
